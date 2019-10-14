b9d946fd5e393a64103b9ce50658071a78f6e57f
Jean Liautaud <jliautaud@free.fr>
2019-10-14 10:25:06 +0200

    changements mineurs

--- contact.php
+++ contact.php
@@ -15,5 +15,5 @@
         </section>
     </main>
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11533.573295250599!2d7.2457434197753905!3d43.723155399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1536332054725"
-            width="100%"" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+            width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 <?php include 'footer.php'; ?>
